6c7298d7803636c0dc0593f0f65fe44698e512df
FACTUM-CLOUD\guillermo.pastor <guillermo.pastor@factum.es>
2018-10-08 10:21:20 +0200

    añadido cambio para pre (ruta del swagger)

--- SmartSignNodeApi/imports/ui/components/swagger/swagger.js
+++ SmartSignNodeApi/imports/ui/components/swagger/swagger.js
@@ -7,7 +7,8 @@ Template.swagger.onCreated(function(){
 });
 Template.swagger.onRendered(function (){
     SwaggerUI({
-        url: 'http://localhost:3000/api/v1/ethereum/swagger.json',
+        // url: 'http://localhost:3000/api/v1/ethereum/swagger.json',
+        url : 'http://10.16.15.235:3000/api/v1/ethereum/swagger.json',
         dom_id: '#swaggerDiv',
         layout: 'BaseLayout'
     }); 
